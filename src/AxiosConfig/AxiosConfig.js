bd8b5f18cb4d6e19a4d1ccbff786aef6fb0b6708
robel167 <robeldemsew1@gmail.com>
2024-07-02 14:24:26 +0300

    commit

--- src/AxiosConfig/AxiosConfig.js
+++ src/AxiosConfig/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
     //baseURL: "http://localhost:5500/api",
-    baseURL: "https://evangadi-forum-backend-14.onrender.com",
+    baseURL: "https://evangadi-forum-backend-k0hd.onrender.com/api",
 });
 
 export default axiosBase;
